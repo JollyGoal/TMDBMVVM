9481fe2cdfcdd2c4463c1dfff6f5dc91ae13576c
JollyGoal <stfill01list.ru@gmail.com>
2020-07-08 00:26:13 +0500

    Adding a single movie activity (2.2)

---
+++ app/src/main/java/com/example/tmdbmvvm/single_movie_details/SingleMovieActivity.kt
@@ -0,0 +1,12 @@
+package com.example.tmdbmvvm.single_movie_details
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.tmdbmvvm.R
+
+class SingleMovieActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_single_movie)
+    }
+}
