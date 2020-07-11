9de619b743607bf055c05e8c2c725880743bcb61
JollyGoal <stfill01list.ru@gmail.com>
2020-07-11 23:58:21 +0500

    Creating UI for popular movies list (3.1)

--- app/src/main/java/com/example/tmdbmvvm/data/api/TheMovieDBInterface.kt
+++ app/src/main/java/com/example/tmdbmvvm/data/api/TheMovieDBInterface.kt
@@ -1,13 +1,18 @@
 package com.example.tmdbmvvm.data.api
 
 import com.example.tmdbmvvm.data.vo.MovieDetails
+import com.example.tmdbmvvm.data.vo.MovieResponse
 import io.reactivex.Single
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface TheMovieDBInterface {
 
+    @GET("movie/popular")
+    fun getPopularMovie(@Query("page") page: Int): Single<MovieResponse>
+
     @GET("movie/{movie_id}")
-    fun getMovieDetails(@Path("movie_id") id:Int): Single<MovieDetails>
+    fun getMovieDetails(@Path("movie_id") id: Int): Single<MovieDetails>
 
 }
